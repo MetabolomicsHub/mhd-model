992ee5db5f08082caaa62a92b3da66c7dbdfcc09
oyurekten <ozgury@ebi.ac.uk>
2025-09-12 09:14:57 +0000

    fix: format updates.

--- mhd_model/__init__.py
+++ mhd_model/__init__.py
@@ -1,12 +1,7 @@
 import pathlib
 import sys
 
-<<<<<<< HEAD
-__version__ = "v0.1.8"
-=======
-
 __version__ = "v0.1.9"
->>>>>>> 582ab5a6f38d964d8479c4756fbd617b22d36477
 
 application_root_path = pathlib.Path(__file__).parent.parent
 
